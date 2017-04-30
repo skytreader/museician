499644f19ec284257d5ad5fa2068822398a8c598
Chad Estioco <chadestioco@gmail.com>
2017-04-30 20:39:06 +0800

    Add app files.

---
+++ app/src/main/java/net/skytreader/museician/HomeChooserActivity.java
@@ -0,0 +1,13 @@
+package net.skytreader.museician;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeChooserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_chooser);
+    }
+}
