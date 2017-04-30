aa4925ac965cfe50276d490c8ed23c252109a353
Chad Estioco <chadestioco@gmail.com>
2017-04-30 22:41:56 +0800

    Life-in-the-fast-lane coding.

---
+++ app/src/main/java/net/skytreader/museician/CountdownPlayActivity.java
@@ -0,0 +1,13 @@
+package net.skytreader.museician;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CountdownPlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_countdown_play);
+    }
+}
--- app/src/main/java/net/skytreader/museician/HomeChooserActivity.java
+++ app/src/main/java/net/skytreader/museician/HomeChooserActivity.java
@@ -1,13 +1,27 @@
 package net.skytreader.museician;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class HomeChooserActivity extends AppCompatActivity {
+
+    public static final String HOME_COUNTDOWN_SECONDS = "net.skytreader.museician.HomeChooserActivity.HOME_COUNTDOWN_SECONDS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_chooser);
+    }
+
+    public void startJamming(View view){
+        // TODO something
+        Intent intent = new Intent(this, CountdownPlayActivity.class);
+        EditText countdownSecondsET = (EditText) findViewById(R.id.countdownSeconds);
+        int countdownSeconds = Integer.parseInt(countdownSecondsET.getText().toString());
+        intent.putExtra(HomeChooserActivity.HOME_COUNTDOWN_SECONDS, countdownSeconds);
+        startActivity(intent);
     }
 }
