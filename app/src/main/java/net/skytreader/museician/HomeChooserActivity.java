de71706232ef391dfef8b8ab0a54324138f9c1a3
Chad Estioco <chadestioco@gmail.com>
2017-05-28 18:57:55 +0800

    Ensure proper breaking of lines.

--- app/src/main/java/net/skytreader/museician/HomeChooserActivity.java
+++ app/src/main/java/net/skytreader/museician/HomeChooserActivity.java
@@ -16,32 +16,42 @@ import com.nbsp.materialfilepicker.MaterialFilePicker;
 
 public class HomeChooserActivity extends AppCompatActivity {
 
-    public static final String HOME_COUNTDOWN_SECONDS = "net.skytreader.museician.HomeChooserActivity.HOME_COUNTDOWN_SECONDS";
+    public static final String HOME_COUNTDOWN_SECONDS = "net.skytreader" +
+            ".museician.HomeChooserActivity.HOME_COUNTDOWN_SECONDS";
     public static final int PERMISSIONS_REQUEST_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_chooser);
-        EditText countdownSecondsET = (EditText) findViewById(R.id.countdownSeconds);
+        EditText countdownSecondsET = (EditText) findViewById(R.id
+                .countdownSeconds);
         countdownSecondsET.setText("4");
-        int permissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int permissionStatus = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
         if (permissionStatus == PackageManager.PERMISSION_DENIED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
 
-            } else{
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, HomeChooserActivity.PERMISSIONS_REQUEST_CODE);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest
+                        .permission.READ_EXTERNAL_STORAGE},
+                        HomeChooserActivity.PERMISSIONS_REQUEST_CODE);
             }
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
-        switch(requestCode){
+    public void onRequestPermissionsResult(int requestCode, String[]
+            permissions, int[] grantResults) {
+        switch (requestCode) {
             case HomeChooserActivity.PERMISSIONS_REQUEST_CODE: {
-                if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                if (grantResults.length > 0 && grantResults[0] !=
+                        PackageManager.PERMISSION_GRANTED) {
                     Context c = getApplicationContext();
-                    Toast t = Toast.makeText(c, "Please grant file access to Museician. Functionality might be limited otherwise.", Toast.LENGTH_SHORT);
+                    Toast t = Toast.makeText(c, "Please grant file access to " +
+                            "Museician. Functionality might be limited " +
+                            "otherwise.", Toast.LENGTH_SHORT);
                     t.show();
                 }
             }
@@ -50,13 +60,17 @@ public class HomeChooserActivity extends AppCompatActivity {
 
     public void startJamming(View view) {
         Intent intent = new Intent(this, CountdownPlayActivity.class);
-        EditText countdownSecondsET = (EditText) findViewById(R.id.countdownSeconds);
-        int countdownSeconds = Integer.parseInt(countdownSecondsET.getText().toString());
-        intent.putExtra(HomeChooserActivity.HOME_COUNTDOWN_SECONDS, countdownSeconds);
+        EditText countdownSecondsET = (EditText) findViewById(R.id
+                .countdownSeconds);
+        int countdownSeconds = Integer.parseInt(countdownSecondsET.getText()
+                .toString());
+        intent.putExtra(HomeChooserActivity.HOME_COUNTDOWN_SECONDS,
+                countdownSeconds);
         startActivity(intent);
     }
 
     public void chooseJamSong(View view) {
-        new MaterialFilePicker().withActivity(this).withRequestCode(1).withHiddenFiles(false).withFilterDirectories(true).start();
+        new MaterialFilePicker().withActivity(this).withRequestCode(1)
+                .withHiddenFiles(false).withFilterDirectories(true).start();
     }
 }
