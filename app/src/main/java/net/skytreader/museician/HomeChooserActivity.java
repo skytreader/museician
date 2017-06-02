3f67aea3792185d4d1d22d9434201a90af26de6e
Chad Estioco <chadestioco@gmail.com>
2017-06-03 05:17:33 +0800

    Attempt to redefine UI behavior...failing.

--- app/src/main/java/net/skytreader/museician/HomeChooserActivity.java
+++ app/src/main/java/net/skytreader/museician/HomeChooserActivity.java
@@ -9,16 +9,18 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
 
 public class HomeChooserActivity extends AppCompatActivity {
 
     public static final String HOME_COUNTDOWN_SECONDS = "net.skytreader" +
             ".museician.HomeChooserActivity.HOME_COUNTDOWN_SECONDS";
-    public static final int PERMISSIONS_REQUEST_CODE = 0;
+    public static final int FILE_READ_REQUEST_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +38,21 @@ public class HomeChooserActivity extends AppCompatActivity {
             } else {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest
                         .permission.READ_EXTERNAL_STORAGE},
-                        HomeChooserActivity.PERMISSIONS_REQUEST_CODE);
+                        HomeChooserActivity.FILE_READ_REQUEST_CODE);
             }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == HomeChooserActivity.FILE_READ_REQUEST_CODE &&
+                resultCode == RESULT_OK){
+            Button chooseAndJam = (Button) findViewById(R.id.chooseAndJam);
+            String filename = data.getStringExtra(FilePickerActivity
+                    .RESULT_FILE_PATH);
+            chooseAndJam.setHint("Jam to: " + filename);
         }
     }
 
@@ -45,7 +60,7 @@ public class HomeChooserActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, String[]
             permissions, int[] grantResults) {
         switch (requestCode) {
-            case HomeChooserActivity.PERMISSIONS_REQUEST_CODE: {
+            case HomeChooserActivity.FILE_READ_REQUEST_CODE: {
                 if (grantResults.length > 0 && grantResults[0] !=
                         PackageManager.PERMISSION_GRANTED) {
                     Context c = getApplicationContext();
