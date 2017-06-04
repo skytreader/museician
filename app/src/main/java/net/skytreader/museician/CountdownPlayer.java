ddb13a23fb1dfdfb500e791084074beaeb7f1b06
Chad Estioco <chadestioco@gmail.com>
2017-06-04 20:37:22 +0800

    Clean-up CountdownPlayer for its new role.

--- app/src/main/java/net/skytreader/museician/CountdownPlayer.java
+++ app/src/main/java/net/skytreader/museician/CountdownPlayer.java
@@ -11,30 +11,14 @@ import android.widget.TextView;
 
 public class CountdownPlayer extends AsyncTask<Void, Void, Void> {
 
-    private TextView statusUpdateElement;
     private Activity activity;
-    private int countTime;
-    private long intervalMillis;
 
-    public CountdownPlayer(Activity a, TextView sue, int countTime, long
-            intervalMillis){
+    public CountdownPlayer(Activity a){
         activity = a;
-        statusUpdateElement = sue;
-        this.countTime = countTime;
-        this.intervalMillis = intervalMillis;
     }
 
     @Override
     protected Void doInBackground(Void... params){
-        try {
-            while (countTime >= 0) {
-                statusUpdateElement.setText(Integer.toString(countTime));
-                countTime--;
-                Thread.sleep(intervalMillis);
-            }
-        } catch(InterruptedException ie){
-            Log.e("countdown", "InterruptedException occurred", ie);
-        }
         return null;
     }
 }
