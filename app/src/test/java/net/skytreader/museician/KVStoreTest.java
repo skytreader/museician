ef333ae519af2b314c99f250300f431976caa21b
Chad Estioco <chadestioco@gmail.com>
2017-06-11 21:51:36 +0800

    This kind of mocking is a mockery.

--- app/src/test/java/net/skytreader/museician/KVStoreTest.java
+++ app/src/test/java/net/skytreader/museician/KVStoreTest.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -19,17 +20,24 @@ public class KVStoreTest {
     @Mock
     private SharedPreferences mockSp;
 
-    private final String MOCK_NAMESPACE = "MOCK_NAMESPACE";
+    private final String NONEXISTENT_KEY = "NONEXISTENT_KEY";
 
     @Test
     public void testKVStore(){
-        KVStore kvs = new KVStore(mockSp);
+        KVStore kvs = createMockedKVStore();
+
         String nonexistent = kvs.get("NONEXISTENT_KEY", null);
         assertEquals(null, nonexistent);
         kvs.set("NONEXISTENT_KEY", "not anymore!");
         assertEquals("not anymore!", kvs.get("NONEXISTENT_KEY", null));
         kvs.set("NONEXISTENT_KEY", "NOT ANYMORE!");
         assertEquals("NOT ANYMORE!", kvs.get("NONEXISTENT_KEY", null));
+    }
+
+    private KVStore createMockedKVStore(){
+        when(mockSp.getString(NONEXISTENT_KEY, null)).thenReturn("not " +
+                "anymore!");
+        return new KVStore(mockSp);
     }
 
 }
