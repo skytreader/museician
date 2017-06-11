e90b329f83ebfadb1ff84218d591220dd5f427b8
Chad Estioco <chadestioco@gmail.com>
2017-06-12 07:00:44 +0800

    Start testing LRUPriorityQueue.

---
+++ app/src/test/java/net/skytreader/museician/LRUPriorityQueueTest.java
@@ -0,0 +1,19 @@
+package net.skytreader.museician;
+
+import android.content.SharedPreferences;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Created by chad on 6/12/17.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class LRUPriorityQueueTest {
+
+    private SharedPreferences mSharedPref;
+    private SharedPreferences.Editor mSharedPrefEditor;
+
+    private final String TEST_NAMESPACE = "TEST_NAMESPACE";
+}
