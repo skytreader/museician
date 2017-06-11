09e24d3c35f04ee3979c3a75dedab3a70ca7a0fd
Chad Estioco <chadestioco@gmail.com>
2017-06-11 23:36:04 +0800

    Some sketches.

---
+++ app/src/main/java/net/skytreader/museician/LRUPriorityQueue.java
@@ -0,0 +1,42 @@
+package net.skytreader.museician;
+
+import android.content.SharedPreferences;
+
+/**
+ * Implements a priority queue that evicts members based on recency of access.
+ * The underlying storage for the queue items is a namespaced SharedPreferences
+ * instance.
+ *
+ * Would have used {@link android.util.LruCache} but then it does not seem to
+ * have some of the functionality I need so this.
+ *
+ * WARNING: Not at all efficient for very large sizes. The intended use case
+ * is here is for up to 4 items, with very little of actual app usage time
+ * touching this code so this should be acceptable.
+ *
+ * Created by chad on 6/11/17.
+ */
+
+public class LRUPriorityQueue {
+
+    private SharedPreferences spStorage;
+    private SharedPreferences.Editor spStorageEditor;
+    private String keyNamespace;
+    private String[] representation;
+
+    public LRUPriorityQueue(SharedPreferences sp, int queueLen, String
+            namespace){
+        spStorage = sp;
+        spStorageEditor = sp.edit();
+        representation = new String[queueLen];
+        keyNamespace = namespace;
+    }
+
+    public int getSize(){
+        return representation.length;
+    }
+
+    public void enqueue(String val){
+
+    }
+}
