a6679f519536ccd8882c81caa8d76a90a656edbc
Chad Estioco <chadestioco@gmail.com>
2017-06-11 23:06:26 +0800

    Make tests pass.
    I kinda get what Mockito's point is now.

--- app/src/test/java/net/skytreader/museician/KVStoreTest.java
+++ app/src/test/java/net/skytreader/museician/KVStoreTest.java
@@ -29,24 +29,21 @@ public class KVStoreTest {
     public void testKVStore(){
         KVStore kvs = createMockedKVStore();
 
-//        String nonexistent = kvs.get("NONEXISTENT_KEY", null);
-//        assertEquals(null, nonexistent);
-//        kvs.set("NONEXISTENT_KEY", "not anymore!");
-//        assertEquals("not anymore!", kvs.get("NONEXISTENT_KEY", null));
-//        kvs.set("NONEXISTENT_KEY", "NOT ANYMORE!");
-//        assertEquals("NOT ANYMORE!", kvs.get("NONEXISTENT_KEY", null));
-        assertEquals(kvs.get("test", null), "yes");
+        String nonexistent = kvs.get(NONEXISTENT_KEY, null);
+        assertEquals(null, nonexistent);
+        when(mockSp.getString(NONEXISTENT_KEY, null)).thenReturn("not " +
+                "anymore!");
+        assertEquals("not anymore!", kvs.get(NONEXISTENT_KEY, null));
+        when(mockSp.getString(NONEXISTENT_KEY, null)).thenReturn("NOT " +
+                "ANYMORE!");
+        assertEquals("NOT ANYMORE!", kvs.get(NONEXISTENT_KEY, null));
     }
 
     private KVStore createMockedKVStore(){
-        //when(mockSp.getString(NONEXISTENT_KEY, null)).thenReturn("not " +
-        //        "anymore!");
         mockSp = mock(SharedPreferences.class);
         mockSpEditor = mock(SharedPreferences.Editor.class);
         when(mockSpEditor.commit()).thenReturn(true);
         when(mockSp.edit()).thenReturn(mockSpEditor);
-        when(mockSp.getString("test", null)).thenReturn("yes");
-        //when(mockSp.getString(NONEXISTENT_KEY, null)).thenCallRealMethod();
         return new KVStore(mockSp);
     }
 
