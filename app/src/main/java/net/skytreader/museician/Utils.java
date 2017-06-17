ac019c6a833eb2f7447c93e89bc18df03c1a3caa
Chad Estioco <chadestioco@gmail.com>
2017-06-18 05:32:22 +0800

    Filter to mp3s only.

--- app/src/main/java/net/skytreader/museician/Utils.java
+++ app/src/main/java/net/skytreader/museician/Utils.java
@@ -4,14 +4,18 @@ import android.app.Activity;
 
 import com.nbsp.materialfilepicker.MaterialFilePicker;
 
+import java.util.regex.Pattern;
+
 /**
  * These are very project-specific utilities. Will mostly contain refactored
  * code.
- *
+ * <p>
  * Created by chad on 6/4/17.
  */
 
 public class Utils {
+
+    public static Pattern MP3_FILES = Pattern.compile(".*\\.mp3");
 
     /**
      * Creates an instance of MaterialFilePicker with sensible defaults.
@@ -24,7 +28,7 @@ public class Utils {
                                                               String lastDirectory) {
         return new MaterialFilePicker().withActivity(a).withRequestCode
                 (PermissionsRequest.FILE_READ)
-                .withHiddenFiles(false).withFilterDirectories(true).withPath
+                .withFilter(MP3_FILES).withHiddenFiles(false).withPath
                         (lastDirectory);
     }
 
